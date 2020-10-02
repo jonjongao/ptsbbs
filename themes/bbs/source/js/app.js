dd356dcf053fce778cdbb53bfbba7055a394c4f4
Jon Gao <jonjon.liho@gmail.com>
2020-10-03 03:36:45 +0800

    done login page with input field and check

--- themes/bbs/source/js/app.js
+++ themes/bbs/source/js/app.js
@@ -1,3 +1,29 @@
+function replace(ak) {
+    $('#mainContainer > span > div > span > span > span').each(function () {
+        var text = $(this).text();
+        $(this).text(text.replace(/\s/g, "<_>"));
+    });
+
+    ak();
+}
+
+function replace2() {
+    $('#mainContainer > span > div > span > span > span').each(function () {
+        var text = $(this).text();
+        $(this).text(text.replace(/<_>/g, " "));
+    });
+}
+
+$(document).ready(function () {
+    /*
+    setTimeout(replace, 0);
+    setTimeout(vueInit, 1000);
+    setTimeout(replace2, 2000);
+    */
+
+    replace(vueInit);
+});
+
 Vue.component('ep2', {
     template: '<div>{{ text }}</div>',
     data() {
@@ -17,28 +43,55 @@ Vue.component('input_code', {
     }
 });
 
-var app = new Vue({
-    el: '.main',
-    data: {
-        loginName: '琪',
-        ctn1: 1,
-        ctn2: 1,
-        ctnActiveClass: 'tabbar_active',
-        ctnDeactiveClass: '',
-        ctnErrorClass: '',
-        trySearch: '',
-        loginAccount: ''
-    },
-    methods: {
-        toggle: function () {
-            this.a1 = !this.a1;
+
+function vueInit() {
+
+    var app = new Vue({
+        el: '.main',
+        data: {
+            accountPlaceholder: '',
+            passwordPlaceholder: '',
+            ctn1: 1,
+            ctn2: 1,
+            ctnActiveClass: 'tabbar_active',
+            ctnDeactiveClass: '',
+            ctnErrorClass: '',
+            trySearch: '',
+            loginAccount: '',
+            loginPassword: ''
         },
-        onLogin: function () {
-            //console.log("prev " + this.$cookies.get("search"));
-            console.log("hey " + this.loginAccount);
-            //this.$cookies.set("search", this.trySearch, 10);
-            
-            this.loginAccount = '';
+        methods: {
+            toggle: function () {
+                this.a1 = !this.a1;
+            },
+            onLogin: function () {
+                //console.log("prev " + this.$cookies.get("search"));
+                console.log("id " + this.loginAccount);
+                //this.$cookies.set("search", this.trySearch, 10);
+
+                //this.loginAccount = '';
+
+                this.$refs.pw.focus();
+            },
+            onPassword: function () {
+                //console.log("prev " + this.$cookies.get("search"));
+                console.log("pw " + this.loginPassword);
+                //this.$cookies.set("search", this.trySearch, 10);
+
+                //this.loginPassword = '';
+                this.chkLogin();
+            },
+            chkLogin: function () {
+                if (this.loginAccount == "charmon" &&
+                    this.loginPassword == "likewhat") {
+                    console.log("success!");
+                    window.location.href = '/ptsbbs/page1';
+                } else {
+                    console.log("failed");
+                }
+            }
         }
-    }
-});
+    });
+
+    replace2();
+}
