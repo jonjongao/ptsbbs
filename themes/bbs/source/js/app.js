3b58e0cd4694abd81a85fe6a919171adc423ed08
jonjongao <jonjon.liho@gmail.com>
2020-10-05 17:32:16 +0800

    fix row pointer, done date label

--- themes/bbs/source/js/app.js
+++ themes/bbs/source/js/app.js
@@ -64,10 +64,33 @@ function addVue(callback) {
             var i = document.getElementById('mainContainer').getAttribute('bbsrowCount');
             this.bbsrowCount = i;
         },
+        computed: {
+            getAccountLabel: function () {
+                //Original 'Charmon' length = 7
+                //Rest of space length = 9, so is 16 in total
+                var t = this.loginAccount;
+                t = t.padEnd(16);
+                return t;
+            },
+            getTimeLabel: function () {
+                //Original is '[9/30 星期三 0:29]' length = 15
+                var now = new Date();
+                var now_local = now.toLocaleDateString();
+                var mon = now_local.split('/')[0];
+                var day = now_local.split('/')[1];
+                var day_list = ['日', '一', '二', '三', '四', '五', '六'];
+                var week = day_list[now.getDay()];
+                var hour = now.getHours();
+                var min = now.getMinutes();
+                var t = '[' + mon + '/' + day + ' 星期' + week + ' ' + hour + ':' + min + ']';
+                t = t.padStart(15);
+                return t;
+            }
+        },
         data: {
             accountPlaceholder: '',
             passwordPlaceholder: '',
-            loginAccount: '',
+            loginAccount: 'Guest',
             loginPassword: '',
             bbsrowIndex: 0,
             bbsrowCount: 0
